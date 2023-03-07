72ba3d6164123d909ea269aa1105f168d87cf90a
QA-Aidan <aidanaedy.qa@gmail.com>
2023-03-07 15:15:14 +0000

    adding in for loop example

--- src/Conditionals_d.java
+++ src/Conditionals_d.java
@@ -8,26 +8,23 @@ public class Conditionals_d {
         Scanner in2 = new Scanner(System.in);
         System.out.println("Please use the +, -, / or * symbol to select the plus, minus, divide or multiply function");
         String symbol = inS.nextLine();
-        System.out.println(symbol);
         System.out.println("Please enter the first number");
         int num1 = in1.nextInt();
         System.out.println("Please enter the second number");
         int num2 = in2.nextInt();
 
-        if (symbol == "+") {
+        if ("+".equals(symbol)) {
             System.out.println("The result is : " + Conditionals_d.add(num1, num2));
-        } else if (symbol == "-") {
+        } else if ("-".equals(symbol)) {
             System.out.println("The result is : " + Conditionals_d.subtract(num1, num2));
-        } else if (symbol == "*") {
+        } else if ("*".equals(symbol)) {
             System.out.println("The result is : " + Conditionals_d.multiply(num1, num2));
-        } else if (symbol == "/") {
+        } else if ("/".equals(symbol)) {
             System.out.println("The result is : " + Conditionals_d.divide(num1, num2));
         } else {
             System.out.println("That is not a valid symbol, select + - * /");
         }
-        inS.close();
-        in1.close();
-        in2.close();
+        inS.close(); in1.close(); in2.close();
     }
 
     public static int add(int number_1, int number_2) {
---
+++ src/For_a.java
@@ -0,0 +1,9 @@
+public class For_a {
+  public static void main(String[] args) {
+    // a simple for loop - usefull when you know the amount
+
+    for (int i = 0; i < 5; i++) {
+      System.out.println(i);
+    }
+  }
+}
